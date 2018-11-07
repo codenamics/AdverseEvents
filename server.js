d51d747d11228fa8ce1555d6b80cedb6a079d7e5
codenamics <info@codenamics.pl>
2018-11-07 23:22:20 +0100

    Setup mailing, security

---
+++ server.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const validatedInput = require('./validation/fields')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.post('/send', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validatedInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const output = `
+    <h1>New Adverse Event reported!</h1>
+    <ul style="list-style:none;">
+        <li>Name: <h3>${req.body.name}</h3></li>
+        <li>Lastname: <h3>${req.body.lastname}</h3></li>
+        <li>Location: <h3>${req.body.location}</h3></li>
+        <li>Phone: <h3>${req.body.phone}</h3></li>
+        <li>Date: <h3>${req.body.date}</h3></li>
+        <li>Drug: <h3>${req.body.drug}</h3> </li>
+        <li>Batch number: <h3>${req.body.batch}</h3></li>
+        <li>Adverse Event Outcome: <h3>${req.body.outcome}</h3></li>
+    </ul>
+    <p>
+    Adverse Event details: <br>
+        ${req.body.details}
+    </p>
+    `
+    let transporter = nodemailer.createTransport({
+        host: process.env.SMTP,
+        port: process.SMTP_PORT,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env.AUTH_USER, // generated ethereal user
+            pass: process.env.AUTH_PASS // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Adverse Event Pharma" <foo@example.com>', // sender address
+        to: 'info@codenamics.pl',
+        subject: `New AE has been reported for ${req.body.drug}`, // Subject line
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.status(200).json({
+            'msg': 'Message has been sent'
+        })
+    });
+})
+
+app.listen(5000, () => {
+    console.log('Server is running')
+})
---
+++ validation/fields.js
@@ -0,0 +1,74 @@
+const Validator = require('validator')
+const isEmpty = require('./is-empty')
+
+const validatedInput = (data) => {
+    let errors = {}
+
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.lastname = !isEmpty(data.lastname) ? data.lastname : '';
+    data.location = !isEmpty(data.location) ? data.location : '';
+    data.phone = !isEmpty(data.phone) ? data.phone : '';
+    data.date = !isEmpty(data.date) ? data.date : '';
+    data.drug = !isEmpty(data.drug) ? data.drug : '';
+    data.batch = !isEmpty(data.batch) ? data.batch : '';
+    data.outcome = !isEmpty(data.outcome) ? data.outcome : '';
+    data.details = !isEmpty(data.details) ? data.details : '';
+
+    if (!Validator.isLength(data.name, {
+            min: 2,
+            max: 30
+        })) {
+        errors.name = 'Name must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required';
+    }
+    if (!Validator.isLength(data.lastname, {
+            min: 2,
+            max: 30
+        })) {
+        errors.lastname = 'Lastname must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.lastname)) {
+        errors.lastname = 'Lastname field is required';
+    }
+
+    if (Validator.isEmpty(data.location)) {
+        errors.location = 'Location field is required';
+    }
+    if (!Validator.isLength(data.phone, {
+            min: 6
+        })) {
+        errors.phone = 'Phone must be between at least 6 characters';
+    }
+
+    if (Validator.isEmpty(data.phone)) {
+        errors.phone = 'Phone field is required';
+    }
+
+    if (Validator.isEmpty(data.date)) {
+        errors.date = 'Date field is required';
+    }
+    if (Validator.isEmpty(data.drug)) {
+        errors.drug = 'Drug field is required';
+    }
+    if (Validator.isEmpty(data.batch)) {
+        errors.batch = 'Batch field is required';
+    }
+    if (Validator.isEmpty(data.outcome)) {
+        errors.outcome = 'Outcome field is required';
+    }
+
+    if (Validator.isEmpty(data.details)) {
+        errors.details = 'Details field is required';
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+
+}
+
+module.exports = validatedInput
