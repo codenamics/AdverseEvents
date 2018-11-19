c5b5613b7f25bec7950da9f7e9ddf621ff386ebe
codenamics <info@codenamics.pl>
2018-11-19 15:52:43 +0100

    center success

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Success() {
   return (
     <div>
-      <h1 className="center-align">Adverse Event Sent!</h1>
+      <h1 className="text-center"> Adverse Event Sent! </h1>{" "}
     </div>
   );
 }
